f4be07ab3d6c6e2781d21a5b8f4f011d9cedb4a4
124110731 ANNE GRAZIELI MARQUES SILVA <anne.grazieli.marques.silva@lcc3-003.lcc.ufcg.edu.br>
2025-07-25 11:11:07 -0300

    first commit

---
+++ src/main/java/adt/stack/StackImpl.java
@@ -0,0 +1,56 @@
+package adt.stack;
+
+public class StackImpl<T> implements Stack<T> {
+
+	private T[] array;
+	private int top;
+
+	@SuppressWarnings("unchecked")
+	public StackImpl(int size) {
+		array = (T[]) new Object[size];
+		top = -1;
+	}
+
+	@Override
+	public T top() {
+		T result = null;
+		if (!isEmpty()) {
+			result = array[top];
+		}
+		return result;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return top == - 1;
+	}
+
+	@Override
+	public boolean isFull() {
+		return top == array.length - 1;
+	}
+
+	@Override
+	public void push(T element) throws StackOverflowException {
+		if (isFull()) {
+			throw new StackOverflowException();
+		} else if (!element.equals(null)) {
+			top ++;
+			array[top] = element;
+		}
+	}
+
+	@Override
+	public T pop() throws StackUnderflowException {
+		T result = null; 
+		if (top == -1) {
+			throw new StackUnderflowException();
+		} else {
+			result = array[top];
+			array[top] = null;
+			top --;
+		}
+		return result;
+	}
+
+}
